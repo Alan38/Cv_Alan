71e5138464d3ee0e9e6df66b883989cf0d5e28e2
Alan38 <alan.lazzarotto@hotmail.fr>
2018-09-25 15:07:49 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Alan Lazzarotto - Curriculum Vitae</title>
+
+  <meta name="viewport" content="width=device-width" />
+  <meta name="description" content="The Curriculum Vitae of Joe Bloggs." />
+  <meta charset="UTF-8">
+
+  <link type="text/css" rel="stylesheet" href="CSS/style.css">
+  <link href='https://fonts.googleapis.com/css?family=Rokkitt:400,700|Lato:400,300|Raleway' rel='stylesheet' type='text/css'>
+</head>
+
+<body id="top">
+  <div id="cv" class="instaFade">
+    <div class="mainDetails">
+      <div id="headshot" class="quickFade">
+        <img src="Img/me.jpg" alt="Alan" />
+      </div>
+
+      <div id="name">
+        <h1 class="quickFade delayTwo">Alan Lazzarotto</h1>
+        <h2 class="quickFade delayThree">Developpeur Web</h2>
+        <h2 class="quickFade delayThree">Junior</h2>
+      </div>
+
+      <div id="contactDetails" class="quickFade delayFour">
+        <ul>
+          <li>6 rue Léon Lambert, 38160, St Marcellin.</li>
+          <li>Email: <a href="mailto:Alan.lazzarotto@hotmail.fr" target="_blank">Alan.lazzarotto@hotmail.fr</a></li>
+          <li>Permis B.</li>
+          <li>24 ans.</li>
+        </ul>
+      </div>
+      <div class="clear"></div>
+    </div>
+
+    <div id="mainArea" class="quickFade delayFive">
+      <section>
+        <article>
+          <div class="sectionCitation">
+            <p>"Si vous avez un travail où il n’y a pas de complications, vous n’avez pas de travail."</p>
+          </div>
+        </article>
+        <div class="clear"></div>
+      </section>
+
+        <section>
+        <div class="sectionTitle">
+          <h1>Formations</h1>
+        </div>
+
+        <div class="sectionContent">
+          <article>
+            <h2>Developpeur Web - Pont en Royans (Develo'Pont)</h2>
+            <p class="subDetails">BAC +2 (en cours) - 2017</p>
+          </article>
+          <br>
+          <article>
+            <h2>Maintenance Véhicules Particuliers - Guynemer</h2>
+            <p class="subDetails">BAC PRO - 2012</p>
+          </article>
+          <br>
+        </div>
+        <div class="clear"></div>
+      </section>  
+
+      <section>
+        <div class="sectionTitle">
+          <h1>Langages et Compétences Clés</h1>
+        </div>
+
+        <div class="sectionContent">
+          <ul class="keySkills">
+            <li>Html/Css</li>
+            <li>Js/Jquery/Angular</li>
+            <li>Php</li>
+            <li>MySql</li>
+            <li>Utilisation de Git</li>
+            <li>Méthode Agile (Scrum)</li>
+            <li>Travaux sous Linux</li>
+            <li>Sait s'autoformer</li>
+            <li>Maitrise de l'anglais</li>
+          </ul>
+        </div>
+        <div class="clear"></div>
+      </section>
+        
+      <section>
+        <div class="sectionTitle">
+          <h1>Personnalité</h1>
+        </div>
+
+        <div class="sectionContent">
+          <ul class="keySkills">
+            <li>Autonome</li>
+            <li>Logique</li>
+            <li>Rigoureux</li>
+            <li>Methodique</li>
+            <li>Curieux</li>
+            <li>Esprit d'analyse</li>
+          </ul>
+        </div>
+          
+        <div class="clear"></div>
+      </section>
+        
+      <section>
+        <div class="sectionTitle">
+          <h1>Travaux Effectués</h1>
+        </div>
+
+        <div class="sectionContent">
+          <article>
+            <h2>Mon Porte-Folio</h2>
+            <p class="subDetails">Pour voir mes travaux efféctués, vous pouvez vous rendre sur mon Porte-Folio :</p>
+            <a href="http://www.developont.fr/~alan/" target="_blank">developont.fr/~alan</a>
+          </article>
+          <br>
+        </div>
+        <div class="clear"></div>
+      </section>  
+          
+      <section>
+        <div class="sectionTitle">
+          <h1>Experiences Professionnelles</h1>
+        </div>
+
+        <div class="sectionContent">
+            
+
+          <article>
+            <h2>Legrand - St Marcellin</h2>
+            <p class="subDetails">Novembre 2017 - Décembre 2017</p>
+            <p>Stage - Réalisation d'une interface web en duo pour configurer un point d'accès wifi</p>
+          </article>
+            
+          <article>
+            <h2>Fromagerie - Tèche</h2>
+            <p class="subDetails">Octobre 2016 - Novembre 2016</p>
+            <p>Manutentionnaire</p>
+          </article>
+
+          <article>
+            <h2>Auto-Distribution - St Marcellin</h2>
+            <p class="subDetails">Janvier 2016</p>
+            <p>Manutentionnaire</p>
+          </article>
+            
+          <article>
+            <h2>Antartic II - Charmes sur Rhône</h2>
+            <p class="subDetails">Juin 2015 - Juillet 2015</p>
+            <p>Manutentionnaire</p>
+          </article>
+
+        </div>
+        <div class="clear"></div>
+      </section>
+
+    </div>
+  </div>
+
+</body>
+
+</html>
